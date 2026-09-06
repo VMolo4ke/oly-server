af2e9c9b4ec2f6f156cf7937325b818ac087a54e
VMolo4ke <gavr.22898@gmail.com>
2026-09-07 01:54:24 +0300

    feat: user module

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,8 +1,15 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { userService } from "./user.service";
 
 export const userController = new Elysia({ prefix: "/user" }).get(
-  "/",
-  async () => {
-    return "Данные пользователя";
+  "",
+  async ({ headers }) => {
+    const userId = headers["x-user-id"];
+    return await userService.getUserInfo(userId);
+  },
+  {
+    headers: t.Object({
+      "x-user-id": t.String({ description: "ID текущего пользователя" }),
+    }),
   },
 );
---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,17 @@
+import { db } from "../../database";
+import { table } from "../../database/schema";
+import { eq } from "drizzle-orm";
+
+export const userService = {
+  async getUserInfo(userId: string) {
+    const userInfo = await db
+      .select({
+        username: table.user.username,
+        email: table.user.email,
+      })
+      .from(table.user)
+      .where(eq(table.user.id, userId));
+
+    return userInfo;
+  },
+};
